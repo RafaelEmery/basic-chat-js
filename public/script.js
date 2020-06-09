2f2c08cb731d3b0127b90bc0fea6b848f88e316a
RafaelEmery <rafael.emerycade@gmail.com>
2020-06-09 17:59:49 -0300

    Changes on front's JS

---
+++ public/script.js
@@ -0,0 +1,35 @@
+var socket = io('http://localhost:3000');
+
+function renderMessage(message) {
+    var html = '<div class="message"><strong>' + message.author + '</strong>: ' + message.message + '</div>';
+    
+    $('.messages').append(html);
+}
+
+socket.on('previousMessages', function(messages) {
+    for (message of messages) {
+        renderMessage(message);
+    }
+});
+
+socket.on('receivedMessage', function(message) {
+    renderMessage(message);
+});
+
+$('#chat').submit(function(event) {
+    event.preventDefault();
+
+    var author = $('input[name=username]').val();
+    var message = $('input[name=message]').val();
+
+    if (author.length && message.length) {
+        var messageObject = {
+            author: author,
+            message: message
+        };
+
+        renderMessage(messageObject);
+
+        socket.emit('sendMessage', messageObject);
+    }
+});
