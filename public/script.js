ff7d479c3ce35bf9833201965c912dfc1f579331
RafaelEmery <rafael.emerycade@gmail.com>
2020-06-15 19:24:39 -0300

    Front kinda OK. Acceptable

--- public/script.js
+++ public/script.js
@@ -1,17 +1,20 @@
 var socket = io('http://localhost:3000');
 
+//Insere em .messages as mensagens
 function renderMessage(message) {
     var html = '<div class="message"><strong>' + message.author + ': </strong> ' + message.message + '</div>';
     
     $('.messages').append(html);
 }
 
+//Retorna todas as mensagens anteriores e insere em .messages
 socket.on('previousMessages', function(messages) {
     for (message of messages) {
         renderMessage(message);
     }
 });
 
+//Exibe a mensagem enviada
 socket.on('receivedMessage', function(message) {
     renderMessage(message);
 });
@@ -26,10 +29,10 @@ $('form').submit(function(event) {
         var messageObject = {
             author: author,
             message: message
-        };
+    }
 
-        renderMessage(messageObject);
+    renderMessage(messageObject);
 
-        socket.emit('sendMessage', messageObject);
+    socket.emit('sendMessage', messageObject);
     }
 });
